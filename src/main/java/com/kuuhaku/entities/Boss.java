f12d29c3b10292b3ff57450e4fd694c9729ed572
Ygimenez <yagogimenez1@hotmail.com>
2023-03-15 17:56:17 -0300

    Initial commit

--- src/main/java/com/kuuhaku/entities/Boss.java
+++ src/main/java/com/kuuhaku/entities/Boss.java
@@ -8,15 +8,15 @@ public class Boss extends Enemy {
 	private boolean left = false;
 
 	public Boss(Game parent) {
-		super(parent, "boss.png", 2000, 5, 3);
+		super(parent, "boss.png", 2000, 6, 3);
 	}
 
 	@Override
 	public void move() {
-		getBounds().translate(left ? -2 : 2, 0);
+		getBounds().translate(left ? -1 : 1, 0);
 
 		Rectangle safe = getParent().getSafeArea();
-		if ((left && getX() <= safe.width / 3) || (!left && getX() >= safe.width / 3 * 2)) {
+		if ((left && getX() <= safe.width / 4) || (!left && getX() >= safe.width / 4 * 3)) {
 			left = !left;
 			getBounds().translate(0, 10);
 		}
